c279da541bd4092fbc3301d5ea932fb96d116f43
“sarang” <sarangsalame12160@gmail.com>
2022-12-28 12:40:35 +0530

    update userprofile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -6,8 +6,8 @@ const UserProfile = () =>{
   const dataVal=useContext(UserContext);
   return (
     <div>
-    <h1 id="name">{dataVal.name}</h1>
-    <h1 id="age">{dataVal.age}</h1>
+    <h1 id="name">Name:- {dataVal.name}</h1>
+    <h1 id="age">Age:- {dataVal.age}</h1>
     </div>
   )
 }
