a2b1a78a4586d6ea3f67f5a224635beddfe4728b
sarang salame <115799065+sarangsalame@users.noreply.github.com>
2022-12-28 12:12:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,18 @@ import React from 'react'
 import '../styles/App.css';
 import { UserProfile } from './UserProfile';
 
-
+export const data = React.createContext();
 const App = () => {
 
   return (
     <div id="main">
-      
+      <data.provider value={name:"Newton", age:3}>
         <UserProfile />
-      
+      </data.provider>
     </div>
   )
 }
 
 
 export default App;
-export {UserContext}
+// export {UserContext}
